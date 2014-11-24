90e721af946c53acb8a8f2444650c025564fe283
freejacque <freejacque@gmail.com>
2014-11-24 13:54:28 -0500

    updated getting cart data to factor in multiples of one item

--- bounce_exchange_code_challenge.js
+++ bounce_exchange_code_challenge.js
@@ -1,15 +1,18 @@
 var dropDownMenu = document.body.querySelectorAll(".dropdown-menu");
 
-var cart = document.body.querySelector("ol#cart-sidebar");
+var cart = document.body.querySelector("ol#cart-sidebar") || document.body.querySelector("div.dropdown is-empty");
 // finds the cart
 var itemsInCart = cart.querySelectorAll("li");
+
 
 // finds the number of items in the cart
 var totalPrice = 0;
 var itemImages = [];
 for (var i = 0, len = itemsInCart.length; i < len; i++) {
   numberOfItems = i + 1;
-  var newPrice = parseFloat(itemsInCart[i].querySelector(".product-details").querySelector(".price").innerHTML.slice(1));
+  var itemPrice = parseFloat(itemsInCart[i].querySelector(".product-details").querySelector(".price").innerHTML.slice(1));
+  var itemTotalNumber = parseFloat(itemsInCart[i].querySelector("strong").innerHTML);
+  var newPrice = itemPrice * itemTotalNumber;
   totalPrice += newPrice;
   cartTotal = "$" + totalPrice;
   itemImages.push(itemsInCart[i].querySelector("a").querySelector("img").src);
@@ -28,12 +31,11 @@ window.onscroll = function() {
   }
 };
 
-// tells the distance from the top of the window
-var position = window.scrollY
-var windowHeight = window.innerHeight
 
+// created the body variable
 var body = document.body;
 
+// creates a scroll event that will be triggered when at the bottom 10% of the page
 window.onscroll = function (){
   console.log("scrolling!");
   var windowHeight = window.innerHeight;
@@ -46,5 +48,18 @@ window.onscroll = function (){
   }
 }
 
+// creating an overlay to be triggered with scroll event
+var overLay = document.createElement("div").addClassName("overlay");
+var innerOverlay = document.createElement("div").addClassName("inner-overlay");
+var overlayContent = document.create
 
-var overLay = document.createElement("div").addClassName()
+var content = "Number of Items in Cart: " +
+              numberOfItems               +
+              "Total Cost: "              +
+              cartTotal;
+
+function showItemPics(){
+  for (var i = 0, var len = itemImages.length; i < len; i++) {
+    document.createElement("img").addClassName("item-img").innerHTML()
+  };
+};
