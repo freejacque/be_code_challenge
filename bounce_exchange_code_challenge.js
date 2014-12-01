f04e63dd3c6ab46a6527361bc35606e428ac1d05
freejacque <freejacque@gmail.com>
2014-12-01 08:44:15 -0500

    overlay only fires if customer is logged in

--- bounce_exchange_code_challenge.js
+++ bounce_exchange_code_challenge.js
@@ -51,7 +51,7 @@ function showItemPics(){
 
 // removes the overlay from the DOM
 function removeOverlay(){
-  this.parentNode.removeChild(this);
+  newPageCover.parentNode.removeChild(newPageCover);
   fired = 0;
 };
 
@@ -87,44 +87,45 @@ function createButtons(){
   };
 };
 
+var isLoggedIn = document.getElementsByClassName("myaccount-logged-in");
+if (isLoggedIn[0].innerHTML == "(Logged In)"){
 // creates a scroll event that will be triggered when at the bottom 10% of the page
-window.onscroll = function (){
-  console.log("scrolling!");
-  var windowHeight = window.innerHeight;
-  var pixelsFromTop = document.body.scrollTop;
-  var bodyHeight = document.body.offsetHeight;
-  var distance = (bodyHeight - windowHeight) * .90;
-  console.log(pixelsFromTop);
-  if (distance < pixelsFromTop && fired == 0) {
-    newPageCover = document.createElement("div").addClassName("page-cover");
-    newPageCover.setAttribute("style", "display:block; position:fixed; padding:0;");
-    newPageCover.style.cssText +=';'+"margin:0; bottom:0; right:0; z-index:999998;";
-    newPageCover.style.cssText +=';'+"width:100%; height:100%; background:rgba(0,0,0,0.8);";
-    newPageCover.setAttribute("id", "addedOverlay");
-    document.body.appendChild(newPageCover);
-    newPageCover.addEventListener("click", removeOverlay);
-    console.log("this works!");
-    overLay = document.createElement("div").addClassName("overlay").addClassName("fancybox-skin");
-    overLay.setAttribute("style", "margin-left:auto; margin-right:auto; width:33em;");
-    overLay.style.cssText +=';'+"height:12em; padding:15px;"
-    overLay.style.cssText +=';'+"box-shadow: 0 10px 25px rgba(0,0,0,0.5);"
-    newPageCover.appendChild(overLay);
-    innerOverlay = document.createElement("div");
-    innerOverlay.setAttribute("style", "overflow:auto; overflowY:scroll; width:30em;");
-    innerOverlay.style.cssText +=';'+"height:auto; font-size:1.1em; margin-left:auto; margin-right:auto;"
-    innerOverlay.style.cssText +=';'+"text-align:center; line-height:2em;"
-    overLay.appendChild(innerOverlay);
-    fired = 1;
-    // takes the variables from the cart and formats the info.
-    if (cart === 0 | cart === null){
-      innerOverlay.innerHTML = "There are no items in your cart.";
-    } else {
-      var contentItems = "There are " + numberOfItems        + " items in your cart. ";
-      var contentCost  = "The total cost of these items is " + cartTotal + ".";
-      innerOverlay.innerHTML = contentItems + contentCost;
-      showItemPics();
+  window.onscroll = function (){
+    console.log("scrolling!");
+    var windowHeight = window.innerHeight;
+    var pixelsFromTop = document.body.scrollTop;
+    var bodyHeight = document.body.offsetHeight;
+    var distance = (bodyHeight - windowHeight) * .90;
+    console.log(pixelsFromTop);
+    if (distance < pixelsFromTop && fired == 0) {
+      newPageCover = document.createElement("div").addClassName("page-cover");
+      newPageCover.setAttribute("style", "display:block; position:fixed; padding:0;");
+      newPageCover.style.cssText +=';'+"margin:0; bottom:0; right:0; z-index:999998;";
+      newPageCover.style.cssText +=';'+"width:100%; height:100%; background:rgba(0,0,0,0.8);";
+      newPageCover.setAttribute("id", "addedOverlay");
+      document.body.appendChild(newPageCover);
+      console.log("this works!");
+      overLay = document.createElement("div").addClassName("overlay").addClassName("fancybox-skin");
+      overLay.setAttribute("style", "margin-left:auto; margin-right:auto; width:33em;");
+      overLay.style.cssText +=';'+"height:12em; padding:15px; top:6em;"
+      overLay.style.cssText +=';'+"box-shadow: 0 10px 25px rgba(0,0,0,0.5);"
+      newPageCover.appendChild(overLay);
+      innerOverlay = document.createElement("div");
+      innerOverlay.setAttribute("style", "overflow:auto; overflowY:scroll; width:30em;");
+      innerOverlay.style.cssText +=';'+"height:auto; font-size:1.1em; margin-left:auto; margin-right:auto;"
+      innerOverlay.style.cssText +=';'+"text-align:center; line-height:2em;"
+      overLay.appendChild(innerOverlay);
+      fired = 1;
+      // takes the variables from the cart and formats the info.
+      if (cart === 0 | cart === null){
+        innerOverlay.innerHTML = "There are no items in your cart.";
+      } else {
+        var contentItems = "There are " + numberOfItems        + " items in your cart. ";
+        var contentCost  = "The total cost of these items is " + cartTotal + ".";
+        innerOverlay.innerHTML = contentItems + contentCost;
+        showItemPics();
+      }
+      createButtons();
     }
-    createButtons();
-  }
+  };
 };
-
